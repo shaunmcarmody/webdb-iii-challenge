71dbabab7828294e63daf2086f09fa04e7bfe983
shaunmcarmody <shaun.carmody@live.co.uk>
2019-03-27 21:42:35 +0000

    allow CRUD operations for cohorts and students

--- cohorts/cohortsModel.js
+++ cohorts/cohortsModel.js
@@ -8,8 +8,68 @@ router.get('/', (req, res) => {
       res.status(200).json(cohorts);
     })
     .catch(err => {
-      console.log(err);
+      res.status(500).json(err);      
+    });
+});
+
+router.post('/', (req, res) => {
+  db('cohorts')
+    .insert(req.body)
+    .then(ids => {
+      db('cohorts')
+        .where({ id: ids[0] })
+        .then(cohort => {
+          res.status(201).json(cohort);
+        })
     })
+    .catch(err => {
+      res.status(500).json(err);      
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db('cohorts')
+    .where({ id })
+    .then(cohort => {
+      res.status(200).json(cohort);
+    })
+    .catch(err => {
+      res.status(500).json(err);      
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  db('cohorts')
+    .where({ id })
+    .update(req.body)
+    .then(resource => {
+      if (!resource) {
+        return res.status(404).json({ message: "ID not found" });
+      }
+      db('cohorts')
+        .where({ id })
+        .then(cohort => {
+          res.status(200).json(cohort);
+        });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.get('/:id/students', (req, res) => {
+  const { id } = req.params;
+  db('cohorts')
+    .innerJoin('students', 'cohorts.id', 'students.cohort_id')
+    .where('cohort_id', id)
+    .then(resources => {
+      res.status(200).json(resources);
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    });
 });
 
 module.exports = router;
--- students/studentsModel.js
+++ students/studentsModel.js
@@ -8,8 +8,55 @@ router.get('/', (req, res) => {
       res.status(200).json(students);
     })
     .catch(err => {
-      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  db('students')
+    .insert(req.body)
+    .then(ids => {
+      db('students')
+        .where({ id: ids[0] })
+        .then(students => {
+          res.status(201).json(students);
+        })
     })
+    .catch(err => {
+      res.status(500).json(err);      
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db('students')
+    .where({ id })
+    .then(student => {
+      res.status(200).json(student);
+    })
+    .catch(err => {
+      res.status(500).json(err);      
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  db('students')
+    .where({ id })
+    .update(req.body)
+    .then(resource => {
+      if (!resource) {
+        return res.status(404).json({ message: "ID not found" });
+      }
+      db('students')
+        .where({ id })
+        .then(student => {
+          res.status(200).json(student);
+        });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
